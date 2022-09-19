d9ef4767e0b1848809c4f642c219072a23959fb5
farrukh-ali-khan <farrukhalikhan7@gmail.com>
2022-09-19 22:52:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+const e = require("express");
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  request = require("request"),
+  https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us9.api.mailchimp.com/3.0/lists/bccaab79b1";
+
+  const options = {
+    method: "POST",
+    auth: "farrukh:5b6d680c8da531c6610b10f4cc1d7a29-us9",
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function (data) {
+      console.log("data submitted");
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express server listening on port" + process.env.PORT || 3000);
+});
