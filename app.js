5ff17091d9183cf345a53bc09f150b1e50f6e01d
farrukh-ali-khan <farrukhalikhan7@gmail.com>
2022-09-19 23:29:27 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require("express"),
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
